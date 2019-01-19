54a3bc98ad0163eb108d93979f9b075571645b56
Aliaksandra Sankova <sandra.sankova@gmail.com>
2019-01-19 16:00:06 +0000

    Custom producer and consumer

---
+++ kafka-experience/src/main/java/com/kafkainaction/chapter2/Consumer.java
@@ -0,0 +1,28 @@
+package com.kafkainaction.chapter2;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+        props.put("group.id", "helloconsumer");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer(props);
+        consumer.subscribe(Arrays.asList("helloworld"));
+        while (true) {
+            ConsumerRecords<String,String> records = consumer.poll(100);
+            for (ConsumerRecord<String,String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+    }
+}
---
+++ kafka-experience/src/main/java/com/kafkainaction/chapter2/Producer.java
@@ -0,0 +1,20 @@
+package com.kafkainaction.chapter2;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args){
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        org.apache.kafka.clients.producer.Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        ProducerRecord producerRecord = new ProducerRecord("helloworld",null, "hello world again!");
+        producer.send(producerRecord);
+        producer.close();
+    }
+}
